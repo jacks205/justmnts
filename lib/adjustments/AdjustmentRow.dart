e5a9f090823f5fe4c7b9f276f950547d8a9a119a
Joshua Herrera <joshz8462@gmail.com>
2020-09-28 21:33:30 -0700

    Implement add option / add stock buttons in adjustments page

---
+++ lib/adjustments/AdjustmentRow.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
+abstract class AdjustmentRow {
+  void save(BuildContext context);
+}
--- lib/adjustments/OptionsRowWidget.dart
+++ lib/adjustments/OptionsRowWidget.dart
@@ -1,29 +1,40 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:justmnts/adjustments/AdjustmentRow.dart';
 
-class OptionsRowWidget extends StatefulWidget {
-  final Color color;
-  const OptionsRowWidget({Key key, this.color}) : super(key: key);
+class OptionsRowWidget extends StatefulWidget implements AdjustmentRow {
+  const OptionsRowWidget({Key key}) : super(key: key);
+
+  static OptionsRowWidgetState of(BuildContext context) =>
+      context.findAncestorStateOfType<OptionsRowWidgetState>();
+
   @override
-  State<StatefulWidget> createState() =>
-      OptionsRowWidgetState(color: this.color);
+  State<StatefulWidget> createState() => OptionsRowWidgetState();
+
+  @override
+  void save(BuildContext context) {
+    OptionsRowWidget.of(context).save(context);
+  }
 }
 
 class OptionsRowWidgetState extends State<OptionsRowWidget> {
-  Color color;
-  OptionsRowWidgetState({this.color});
+  Color _color;
+  DateTime _expirationDate = DateTime.now();
+  int _strikePrice = 0;
+  int _quantity = 0;
+  int _price = 0;
 
-  DateTime _selectedDate = DateTime.now();
+  void save(BuildContext context) {}
 
   Future<Null> _selectDate(BuildContext context) async {
     final DateTime picked = await showDatePicker(
         context: context,
-        initialDate: _selectedDate,
+        initialDate: _expirationDate,
         firstDate: DateTime(2015, 8),
         lastDate: DateTime(2101));
-    if (picked != null && picked != _selectedDate) {
+    if (picked != null && picked != _expirationDate) {
       setState(() {
-        _selectedDate = picked;
+        _expirationDate = picked;
       });
     }
   }
@@ -32,7 +43,7 @@ class OptionsRowWidgetState extends State<OptionsRowWidget> {
   Widget build(BuildContext context) {
     return Container(
         height: 60,
-        color: this.color,
+        color: this._color,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -42,34 +53,46 @@ class OptionsRowWidgetState extends State<OptionsRowWidget> {
                     padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
                     child: MaterialButton(
                         child: Text(
-                          "${_selectedDate.month}-${_selectedDate.day}-${_selectedDate.year}",
+                          "${_expirationDate.month}-${_expirationDate.day}-${_expirationDate.year}",
                           style: TextStyle(fontSize: 14),
                           key: Key("dateText"),
                         ),
                         onPressed: () => _selectDate(context)))),
             Flexible(
                 flex: 1,
-                child: TextField(
+                child: TextFormField(
+                    initialValue: _strikePrice.toString(),
                     keyboardType: TextInputType.number,
+                    onChanged: (value) {
+                      _strikePrice = int.parse(value);
+                    },
                     decoration: InputDecoration(
                       hintText: "Strike price",
                       labelText: "Strike price",
                     ))),
             Flexible(
                 flex: 1,
-                child: TextField(
+                child: TextFormField(
+                    initialValue: _quantity.toString(),
                     keyboardType: TextInputType.number,
+                    onChanged: (value) {
+                      _quantity = int.parse(value);
+                    },
                     decoration: InputDecoration(
                       hintText: "Quantity",
                       labelText: "Quantity",
                     ))),
             Flexible(
                 flex: 1,
-                child: TextField(
+                child: TextFormField(
+                    initialValue: _price.toString(),
                     keyboardType: TextInputType.number,
+                    onChanged: (value) {
+                      _price = int.parse(value);
+                    },
                     decoration: InputDecoration(
-                      labelText: "Amount",
-                      hintText: "Amount",
+                      labelText: "Price",
+                      hintText: "Price",
                     ))),
           ],
         ));
